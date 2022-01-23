4cce998595f8a0b379c3f220fe904c74dc0b1870
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-01-23 18:57:10 +0200

    add solution task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+window.addEventListener("click", windowClickHandler);
+
+
+
+function createGalleryMarkup (galleryElements) {
+  return galleryElements.map(({ original, preview, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  }).join("");
+}
+function windowClickHandler(event) {
+  if (!event.target.classList.contains('gallery__image')) return;
+  event.preventDefault();
+  const clickImg = {
+    src: event.target.dataset.source,
+    alt: event.target.alt
+  }
+  const instance = basicLightbox.create(
+    `<img src="${clickImg.src}" alt="${clickImg.alt}" />`, {
+      closable: true,
+      className: 'my-class',
+      onShow: (instance) => {
+        // console.log("modal open");
+        window.addEventListener("keydown", pressEscHandler);
+      },
+      onClose: (instance) => {
+        // console.log("modal close");
+        window.removeEventListener("keydown", pressEscHandler);
+      }
+    },
+  )
+  instance.show();
+  function pressEscHandler(event) {
+  if (event.code === "Escape") {
+    // console.log("press esc");
+    instance.close();
+    }
+  } 
+}
